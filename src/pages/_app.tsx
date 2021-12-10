8739f5569f0439db0536da7ce83a49cabea77ad1
yekdagets <73423371+yekdagets@users.noreply.github.com>
2021-12-10 14:55:40 +0300

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,15 +2,13 @@ import React from 'react';
 
 import '../styles/main.css';
 
-import About from '../components/About';
-import Analytics from '../components/Analytics';
+
 import Canvas from '../components/Canvas';
-import Features from '../components/Features';
+
 import Header from '../components/Header';
 import LazyShow from '../components/LazyShow';
 import MainHero from '../components/MainHero';
 import MainHeroImage from '../components/MainHeroImage';
-import Pricing from '../components/Pricing';
 import Product from '../components/Product';
 
 const App = () => {
